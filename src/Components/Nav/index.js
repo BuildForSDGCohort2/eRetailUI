dacb50b7d57a5119fdd19afabc4429e6c6671b18
mariamiah <mariah.kakande@gmail.com>
2020-09-08 12:07:35 +0300

    configure prettier

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -2,19 +2,33 @@
 import React from "react";
 
 export const Nav = () => (
-    <nav id="navigation">
+  <nav id="navigation">
     <div className="container">
-        <div id="responsive-nav">
-            <ul className="main-nav nav navbar-nav">
-                <li className="active"><a href="/">Home</a></li>
-                <li><a href="/">Hot Deals</a></li>
-                <li><a href="/">Categories</a></li>
-                <li><a href="/">Laptops</a></li>
-                <li><a href="/">Smartphones</a></li>
-                <li><a href="/">Cameras</a></li>
-                <li><a href="/">Accessories</a></li>
-            </ul>
-        </div>
+      <div id="responsive-nav">
+        <ul className="main-nav nav navbar-nav">
+          <li className="active">
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Hot Deals</a>
+          </li>
+          <li>
+            <a href="/">Categories</a>
+          </li>
+          <li>
+            <a href="/">Laptops</a>
+          </li>
+          <li>
+            <a href="/">Smartphones</a>
+          </li>
+          <li>
+            <a href="/">Cameras</a>
+          </li>
+          <li>
+            <a href="/">Accessories</a>
+          </li>
+        </ul>
+      </div>
     </div>
-</nav>
+  </nav>
 );
